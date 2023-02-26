e6d3cf3da0acb7a1ca9e269ed08e3e6de84ed6b4
Maxim Fomov <maksimfomov26@gmail.com>
2023-02-26 13:34:20 +0300

    feat: Added constants for listeners

--- src/main/java/by/htp/ex/controller/listeners/AppContextListener.java
+++ src/main/java/by/htp/ex/controller/listeners/AppContextListener.java
@@ -6,12 +6,14 @@ import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 
 public class AppContextListener implements ServletContextListener {
+	private static final String ERROR_MESSAGE = "error during init of connection pool";
+	
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         try {
             ConnectionPool.getInstance().initPoolData();
         } catch (ConnectionPoolException e) {
-        	throw new RuntimeException("error during init of connection pool", e);
+        	throw new RuntimeException(ERROR_MESSAGE, e);
         }
         
         ServletContextListener.super.contextInitialized(sce);
